40c7698a5ed1d4cbd6118c3c24588213d6d3221f
VladRaciulete <vladraciulete@yahoo.com>
2026-02-12 02:49:02 +0200

    feat: implement factories and seeders

--- src/app/Models/Company.php
+++ src/app/Models/Company.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Str;
 
 class Company extends Model
 {
+    use HasFactory;
     protected $fillable = ['name', 'slug'];
 
     protected static function booted(): void
@@ -19,7 +21,7 @@ class Company extends Model
         });
     }
 
-    public function users(): hasMany
+    public function users(): HasMany
     {
         return $this->hasMany(User::class);
     }
--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Company;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,11 +16,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        Company::factory(10)
+            ->hasUsers(5)
+            ->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Admin User',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('password'),
+            'company_id' => Company::factory(),
         ]);
     }
 }
